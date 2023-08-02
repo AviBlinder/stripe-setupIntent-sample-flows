22bf5a9b31238e22a9ca7e48754bff9937eb528d
Avi <aviblinder@gmail.com>
2023-08-02 14:37:13 +0300

    functions

--- stripe-setupIntent-sample-flows/src/views/SuccessSetupIntent.vue
+++ stripe-setupIntent-sample-flows/src/views/SuccessSetupIntent.vue
@@ -2,20 +2,20 @@
   <div>
       Congrats! Setup Intent successfully created
 
-      {{ setup_intent }}
+      <!-- {{ setup_intent }}
 
-      {{ setup_intent_client_secret }}
+      {{ setup_intent_client_secret }} -->
   </div>
 </template>
 
 <script setup lang="ts">
 
-import {ref} from 'vue'
-import { useRouter, useRoute } from 'vue-router'
+// import {ref} from 'vue'
+// import { useRouter, useRoute } from 'vue-router'
 
-  const route = useRoute();
-  const setup_intent  = ref(route.query.setup_intent)
-  const setup_intent_client_secret = ref(route.query.setup_intent_client_secret)
+//   const route = useRoute();
+//   const setup_intent  = ref(route.query.setup_intent)
+//   const setup_intent_client_secret = ref(route.query.setup_intent_client_secret)
 
   
 
