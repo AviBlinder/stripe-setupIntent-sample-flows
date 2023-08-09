c09b0fc3dbe1df2ac7f08b3226be7c77c3fcdb71
Avi <aviblinder@gmail.com>
2023-08-09 19:07:11 +0300

    ...

--- stripe-setupIntent-project/tailwind.config.js
+++ stripe-setupIntent-project/tailwind.config.js
@@ -131,14 +131,14 @@ module.exports = {
 
       // customized additional font-sizes
       fontSize: {
-        // xs: ['14px',{lineHeight: '24px', letterSpacing: '-0.03em'}]
-        // sm: ['16px',{lineHeight: '28px', letterSpacing: '-0.03em'}]
-        // lg: ['18px',{lineHeight: '28px', letterSpacing: '-0.03em'}]
-        // xl: ['24px',{lineHeight: '36px', letterSpacing: '-0.03em'}]
-        // 2xl: ['36px',{lineHeight: '48px', letterSpacing: '-0.032em'}]
-        // 3xl: ['48px',{lineHeight: '56px', letterSpacing: '-0.032em'}]
-        // 4xl: ['56px',{lineHeight: '64px', letterSpacing: '-0.032em'}]
-        // 5xl: ['80px',{lineHeight: '80px', letterSpacing: '-0.032em'}]
+        // xs: ['14px',{lineHeight: '24px', letterSpacing: '-0.03em'}],
+        // sm: ['16px',{lineHeight: '28px', letterSpacing: '-0.03em'}],
+        // lg: ['18px',{lineHeight: '28px', letterSpacing: '-0.03em'}],
+        // xl: ['24px',{lineHeight: '36px', letterSpacing: '-0.03em'}],
+        // '2xl': ['36px',{lineHeight: '48px', letterSpacing: '-0.032em'}],
+        // '3xl': ['48px',{lineHeight: '56px', letterSpacing: '-0.032em'}],
+        // '4xl': ['56px',{lineHeight: '64px', letterSpacing: '-0.032em'}],
+        // '5xl': ['80px',{lineHeight: '80px', letterSpacing: '-0.032em'}]
         '2xl': [
           '24px',
           { lineHeight: '2rem', letterSpacing: '-0.01em', fontWeight: '500' },
